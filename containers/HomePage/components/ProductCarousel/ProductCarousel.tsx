a46b65138772bb6c9db31496238e281ff794a6f4
bylko.aleksey <vyacheslav.malykhin@anuitex.net>
2021-07-09 17:45:03 +0300

    Update responsive

--- containers/HomePage/components/ProductCarousel/ProductCarousel.tsx
+++ containers/HomePage/components/ProductCarousel/ProductCarousel.tsx
@@ -3,6 +3,8 @@ import s from './ProductCarousel.module.css'
 import { Swiper, SwiperSlide } from 'swiper/react'
 import SwiperCore, { Autoplay, Navigation } from 'swiper/core'
 import ProductCard from '../../../../components/ProductCard/ProductCard'
+import useMediaQuery from '../../../../common/hooks/useMediaQuery'
+import { mobileOnly, tabOnly } from '../../../../styles/mediaQuery'
 
 // install Swiper modules
 SwiperCore.use([Autoplay, Navigation])
@@ -10,18 +12,22 @@ SwiperCore.use([Autoplay, Navigation])
 interface ProductCarouselProps {}
 
 const ProductCarousel: React.FC<ProductCarouselProps> = () => {
+  const mobile = useMediaQuery(mobileOnly)
+  const tab = useMediaQuery(tabOnly)
+
   return (
     <div className={s.root}>
       <h2 className={s.title}>Карусель товаров</h2>
       <Swiper
         speed={1000}
+        spaceBetween={tab && mobile ? 24 : 32}
         loop={true}
         navigation={true}
         autoplay={{
           delay: 3000,
           disableOnInteraction: false,
         }}
-        slidesPerView={4}
+        slidesPerView={mobile ? 1 : tab ? 2 : 4}
       >
         <SwiperSlide>
           <ProductCard />
