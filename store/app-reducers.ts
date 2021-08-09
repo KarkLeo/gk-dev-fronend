c2db57b2665934c09cde9cfa0c1742df05116f6d
Vyacheslav <v.malykhin.ff@gmail.com>
2021-08-09 23:50:54 +0300

    update auth store

--- store/app-reducers.ts
+++ store/app-reducers.ts
@@ -1,8 +1,12 @@
 import { combineReducers } from 'redux'
 import { authReducer } from './auth'
+import { modalReducer } from './modal'
+import { profileReducer } from './profile'
 
 const appReducers = combineReducers({
   auth: authReducer,
+  modal: modalReducer,
+  profile: profileReducer,
 })
 
 export default appReducers
--- store/auth/thunks.ts
+++ store/auth/thunks.ts
@@ -1,10 +1,62 @@
 import { AppThunk } from '../types'
-import { UserRegister } from 'services/public'
+import {
+  UserAuthResponse,
+  UserLogin,
+  UserMe,
+  UserRegister,
+} from 'services/public'
 import { publicServices } from 'services'
+import { removeJwt, setJwt } from 'common/jwtService'
+import { setAuthAction } from './actions'
+import { closeModalAction } from '../modal'
+import { setProfileAction } from '../profile'
+
+export const appAuthThunk =
+  (auth: UserAuthResponse): AppThunk =>
+  (dispatch) => {
+    setJwt(auth.jwt)
+    dispatch(setAuthAction(auth.user.id))
+    dispatch(
+      setProfileAction({
+        first_name: auth.user.first_name,
+        last_name: auth.user.last_name,
+        phone_number: auth.user.phone_number,
+        email: auth.user.email,
+      })
+    )
+    dispatch(closeModalAction())
+  }
 
 export const registerThunk =
   (user: UserRegister): AppThunk =>
-  async () => {
-    const res = await publicServices.register(user)
-    console.log(res)
+  async (dispatch) => {
+    try {
+      const res = await publicServices.register(user)
+      dispatch(appAuthThunk(res))
+    } catch (e) {
+      console.log(e)
+    }
+  }
+
+export const loginThunk =
+  (user: UserLogin): AppThunk =>
+  async (dispatch) => {
+    try {
+      const res = await publicServices.login(user)
+      dispatch(appAuthThunk(res))
+    } catch (e) {
+      console.log(e)
+    }
+  }
+
+export const meThunk =
+  (user: UserMe): AppThunk =>
+  async (dispatch) => {
+    try {
+      const res = await publicServices.me(user)
+      dispatch(appAuthThunk(res))
+    } catch (e) {
+      removeJwt()
+      console.log(e)
+    }
   }
