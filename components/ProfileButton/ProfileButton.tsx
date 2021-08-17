8bde899ee9a263c078f35b2b3443e143f915fe1d
slava-malykhin <slava.malykhin@placense.com>
2021-08-18 01:14:07 +0300

    update profile button - add authed state

--- components/ProfileButton/ProfileButton.tsx
+++ components/ProfileButton/ProfileButton.tsx
@@ -1,8 +1,12 @@
 import React, { useCallback } from 'react'
 import Icon from '../Icon'
 import s from './ProfileButton.module.css'
-import { useDispatch } from 'react-redux'
+import { useDispatch, useSelector } from 'react-redux'
 import { openLoginModalAction } from '../../store/modal'
+import { getProfileInitialsSelector } from '../../store/profile'
+import Link from 'next/link'
+import { PROFILE_PAGE_URL } from '../../route'
+import { getIsAuthSelector } from '../../store/auth'
 
 interface ProfileButtonProps {
   iconClassName: string
@@ -11,12 +15,27 @@ interface ProfileButtonProps {
 const ProfileButton: React.FC<ProfileButtonProps> = ({ iconClassName }) => {
   const dispatch = useDispatch()
 
+  const initials = useSelector(getProfileInitialsSelector)
+  const isAuth = useSelector(getIsAuthSelector)
+
   const loginHandler = useCallback(
     () => dispatch(openLoginModalAction()),
     [dispatch]
   )
 
-  return (
+  return isAuth ? (
+    initials ? (
+      <Link href={PROFILE_PAGE_URL}>
+        <a className={s.profile}>{initials}</a>
+      </Link>
+    ) : (
+      <Link href={PROFILE_PAGE_URL}>
+        <a className={iconClassName}>
+          <Icon iconId='user' className={s.profile__icon} />
+        </a>
+      </Link>
+    )
+  ) : (
     <button className={s.button} onClick={loginHandler}>
       <Icon iconId='user' className={iconClassName} />
     </button>
