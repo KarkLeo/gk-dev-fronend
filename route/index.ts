f3eb3c959e801cfaa8814bbbc8cc7d2837550830
slava-malykhin <slava.malykhin@placense.com>
2021-08-18 01:16:12 +0300

    add new routes

--- route/index.ts
+++ route/index.ts
@@ -1,6 +1,14 @@
+export const HOME_PAGE_URL = '/' as const
 export const CONTACT_PAGE_URL = '/contacts' as const
 
 export const CATEGORY_PAGE_URL = '/[category]' as const
 export const CATEGORY_PAGE_PAGINATION_URL = '/[category]/[page]' as const
 
 export const PRODUCT_PAGE_URL = '/[category]/product/[product]' as const
+
+export const PROFILE_PAGE_URL = '/profile' as const
+export const PROFILE_ADDRESS_PAGE_URL = '/profile/address' as const
+export const PROFILE_ORDERS_PAGE_URL = '/profile/orders' as const
+export const PROFILE_PASSWORD_PAGE_URL = '/profile/password' as const
+
+export const CHECKOUT_PAGE_URL = '/checkout' as const
