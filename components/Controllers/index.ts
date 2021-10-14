e9693b6cf8128ddc376eefd644a4e8ec37df187b
slava-malykhin <slava.malykhin@placense.com>
2021-10-15 02:06:39 +0300

    Update address

--- components/Controllers/index.ts
+++ components/Controllers/index.ts
@@ -1,3 +1,4 @@
 export * from './component/TextField/TextField'
 export * from './component/Button/Button'
 export * from './component/Checkbox/Checkbox'
+export * from './component/TextSwitcher/TextSwitcher'
